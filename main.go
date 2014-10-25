1037ecc38acd9111afd1255a62b8e73c707cbac2
Dimitri Walters <me@dimitriwalters.com>
2014-10-25 10:31:40 -0400

    Serve html files

--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "net/http"
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.ListenAndServe(":8080", nil)
 }
